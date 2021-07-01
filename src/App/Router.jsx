e44f672e0bd40bba5030cfbac9e9e598720f1b44
Leona <leonad308@gmail.com>
2021-07-01 11:22:01 -0400

    added react router dom

--- src/App/Router.jsx
+++ src/App/Router.jsx
@@ -21,7 +21,6 @@ export function Router() {
         <Switch>
           <Route path={routes.about.link} component={AboutPage} />
           <Route path={routes.services.link} component={ServicesPage} />
-          {/* <Route path={routes.pricing.link} component={PricingPage} /> */}
           <Route path={routes.faq.link} component={FaqPage} />
           <Route path={routes.contact.link} component={ContactPage} />
           <Route exact path="/" component={HomePage} />
