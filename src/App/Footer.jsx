40b14cd376fbf40a56e145a12005f513da34642d
mamaducks <leonad308@gmail.com>
2021-08-23 10:47:42 -0400

    change icon add redirect footer edits

--- src/App/Footer.jsx
+++ src/App/Footer.jsx
@@ -1,9 +1,5 @@
 import { useMediaQuery } from "react-responsive";
-import {
-  AppBar,
-  Box,
-  Typography,
-} from "@material-ui/core";
+import { AppBar, Box, Typography } from "@material-ui/core";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MailTo } from "../Components/Social/MailTo";
 import { Schedule } from "../Components/Schedule/Schedule";
@@ -12,7 +8,7 @@ import House from "../img/littleHouse.png";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { AiOutlineSchedule } from "react-icons/ai";
 import React from "react";
-import {DialogButton} from "../Components/Schedule/RequestsDialogButtons";
+import { DialogButton } from "../Components/Schedule/RequestsDialogButtons";
 
 export function FootQuoteButtonNew({ title, titleWide, content }) {
   const isWide = useMediaQuery({
@@ -31,88 +27,110 @@ export const Footer = () => {
   });
   return (
     <>
-      <AppBar position="sticky" elevation={0} >
-        <Box display="flex" color="#a71976" alignItems="center" flexWrap="wrap" alignContent="center">
+      <AppBar position="sticky" elevation={0}>
+        <Box
+          display="flex"
+          color="#a71976"
+          alignItems="center"
+          flexWrap="wrap"
+          alignContent="center"
+        >
           <Box textAlign="center" flex="auto" paddingTop="10px">
-            <Typography variant="subtitle1" paddingTop="8px" paddingBottom="1px">
+            <Typography
+              variant="subtitle1"
+              paddingTop="8px"
+              paddingBottom="1px"
+            >
               BUYER BEWARE HOME INSPECTION
-              </Typography>
-              <Typography variant="subtitle2" lineHeight="1.4">
-                Proudly Serving South Jersey
-              </Typography>
-              <Typography variant="caption" >
-                Burlington, Camden, Gloucester, Cumberland, Atlantic, and Cape
-                May Counties
-              </Typography>
+            </Typography>
+            <Typography variant="subtitle2" lineHeight="1.4">
+              Proudly Serving South Jersey
+            </Typography>
+            <Typography variant="caption">
+              Burlington, Camden, Gloucester, Cumberland, Atlantic, and Cape May
+              Counties
+            </Typography>
           </Box>
 
-          <Box margin="5px" display="flex" flex="auto" text-align="center" style={{justifyContent:"center"}}>
-              {isWide && (
-                <img
-                  alt=""
-                  src={House}
-                  width="165px"
-                  maxHeight="100px"
-                  style={{ paddingInline: "5px" }}
-                />
-              )}
-              <Box
-                display="flex"
-                style={{ flexDirection: "column", justifyContent: "center" }}
-              >
-                <FootQuoteButtonNew
-                  title={<RiMoneyDollarCircleLine />}
-                  titleWide="Get a Quote"
-                  content={<Quote />}
-                  contentWide={<Quote />}
-                />
-                <FootQuoteButtonNew
-                  title={<AiOutlineSchedule />}
-                  titleWide="Schedule Online"
-                  content={<Schedule />}
-                  contentWide={<Schedule />}
-                />
-              </Box>
+          <Box
+            margin="5px"
+            display="flex"
+            flex="auto"
+            text-align="center"
+            style={{ justifyContent: "center" }}
+          >
+            {isWide && (
+              <img
+                alt=""
+                src={House}
+                width="165px"
+                maxHeight="100px"
+                style={{ paddingInline: "5px" }}
+              />
+            )}
+            <Box
+              display="flex"
+              style={{ flexDirection: "column", justifyContent: "center" }}
+            >
+              <FootQuoteButtonNew
+                title={<RiMoneyDollarCircleLine />}
+                titleWide="Get a Quote"
+                content={<Quote />}
+                contentWide={<Quote />}
+              />
+              <FootQuoteButtonNew
+                title={<AiOutlineSchedule />}
+                titleWide="Schedule Online"
+                content={<Schedule />}
+                contentWide={<Schedule />}
+              />
+            </Box>
           </Box>
-            <Box display="flex" flex="auto" color="#c54097" justifyContent="center" flexDirection="column" alignItems="center">
-              <Typography
-                variant="body1"
-                
+          <Box
+            display="flex"
+            flex="auto"
+            color="#c54097"
+            justifyContent="center"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Typography
+              variant="body1"
+              style={{
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <FaPhoneAlt
+                fontSize="1.1em"
                 style={{
-                  alignItems: "center",
+                  paddingInline: "8px",
                   display: "flex",
+                  alignItems: "center",
                 }}
-              >
-                <FaPhoneAlt
-                  fontSize="1.1em"
-                  style={{
-                    paddingInline: "8px",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                />
-                1-609-670-0451
-              </Typography>
-            
-              <Typography
-                variant="body1"
-                gutterBottom
+              />
+              1-609-670-0451
+            </Typography>
+
+            <Typography
+              variant="body1"
+              gutterBottom
+              style={{
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <MailTo
                 style={{
-                  alignItems: "center",
+                  color: "#c54097",
+                  fontSize: "1.1em",
+                  paddingInline: "8px",
                   display: "flex",
+                  alignItems: "center",
                 }}
-              >
-                <MailTo
-                  style={{
-                    color: "#c54097",
-                    fontSize: "1.1em",
-                    paddingInline: "8px",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                />
-                Send us an email
-              </Typography>
+              />
+              Send us an email
+            </Typography>
           </Box>
         </Box>
       </AppBar>
